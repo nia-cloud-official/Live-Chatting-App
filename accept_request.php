40551b6fc76d47df21e8ceb0a278f68842fbde8a
Milton Vafana <niacloudofficial@gmail.com>
2024-07-03 15:43:34 +0100

    modified:   accept_request.php, changed the     code to object-oriented programming.

--- accept_request.php
+++ accept_request.php
@@ -1,11 +1,32 @@
-<?php
-session_start();
-include 'connection.php';
-$outgoing_id=$_SESSION['u_id'];
-$chatid=$_GET['id'];
-$query="update friends set status='Yes' where incoming_id='$chatid' and outgoing_id='$outgoing_id'";
-if($res=mysqli_query($conn,$query))
-{
-	header("Location: find.php");
+<?php 
+
+class ChatManager {
+    private $pdo;
+
+    public function __construct($dsn, $username, $password) {
+        try {
+            $this->pdo = new PDO($dsn, $username, $password);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function updateChatStatus($outgoingId, $chatId) {
+        $query = "UPDATE friends SET status = 'Yes' WHERE incoming_id = :chatId AND outgoing_id = :outgoingId";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindParam(':chatId', $chatId);
+        $stmt->bindParam(':outgoingId', $outgoingId);
+
+        if ($stmt->execute()) {
+            header("Location: find.php");
+        }
+    }
 }
-?>
+
+// Usage
+$dsn = 'mysql:host=localhost;dbname=my_database';
+$username = 'root';
+$password = 'password';
+
+$chatManager = new ChatManager($dsn, $username, $password);
+$chatManager->updateChatStatus($_SESSION['u_id'], $_GET['id']);
